cb2d9624738fa01135c6f027151fe82e0277de6b
Marcelo <j.marcelo.pinto.98@gmail.com>
2022-05-01 13:47:48 +0100

    Improved plot
    - Plot now better displays the evolution of the P(H) when increasing the sample size

--- 1-Probability/21.py
+++ 1-Probability/21.py
@@ -10,52 +10,55 @@ sample_size = 1000
 heads_probability = 0.3
 
 # Dataframe identification of the columns used
-SAMPLE_SIZE_COLUMN_NAME = 'SIZE'
-HEADS_PROBABILITY_COLUMN_NAME = 'HEADS_P'
-HEADS_COLUMN_NAME = 'H'
-TAILS_COLUMN_NAME = 'T'
+SAMPLE_SIZE_COLUMN_NAME = 'Sample Size'
+HEADS_PROBABILITY_COLUMN_NAME = 'Probability of Heads'
+HEADS_COLUMN_NAME = 'P(H)'
+TAILS_COLUMN_NAME = 'P(T)'
 
 
 def plot_tosses(data_frame):
-    # Construct the bar chart
+    # Construct and plot the chart
     ax = data_frame.plot(x=SAMPLE_SIZE_COLUMN_NAME,
-                         y=[HEADS_COLUMN_NAME, TAILS_COLUMN_NAME],
+                         y=[HEADS_COLUMN_NAME],
                          ylim=(0, 1),
-                         kind="bar",
-                         title=f"{HEADS_COLUMN_NAME}/{TAILS_COLUMN_NAME} Proportions for {HEADS_COLUMN_NAME} probability of {heads_probability}")
+                         lw=1,
+                         title=f"Evolution of {HEADS_COLUMN_NAME} with increase in sample size")
 
-    # Add a note to each bar for better readability
-    y_note_adjustment_multiplier = 1.005
-    for p in ax.patches:
-        ax.annotate(str(p.get_height()),
-                    (p.get_x() + p.get_width()/2, p.get_height()
-                     * y_note_adjustment_multiplier),
-                    ha='center')
+    ax.axhline(y=data_frame[HEADS_PROBABILITY_COLUMN_NAME].values[0], xmin=0, xmax=data_frame[SAMPLE_SIZE_COLUMN_NAME].values[0],
+               color='r', linestyle='--', lw=2, label=f"{HEADS_COLUMN_NAME}={data_frame[HEADS_PROBABILITY_COLUMN_NAME].values[0]}")
 
+    plt.legend()
     plt.show()
 
 
 def simulate_tosses(sample_size, heads_probability):
-    # toss a coin sample_size times and save the count obtained for each coin side in a dictionary
-    results = {
-        SAMPLE_SIZE_COLUMN_NAME: sample_size,
-        HEADS_PROBABILITY_COLUMN_NAME: heads_probability,
-        HEADS_COLUMN_NAME: 0,
-        TAILS_COLUMN_NAME: 0,
-    }
-    for x in range(1, sample_size + 1):
-        toss_result = coin_toss(heads_probability)
-        if(toss_result == HEADS_COLUMN_NAME):
-            results[HEADS_COLUMN_NAME] += 1
-        else:
-            results[TAILS_COLUMN_NAME] += 1
+    # toss a coin sample_size times and save the count obtained for each coin side in a dictionary, for each size
+    results = []
+    for current_sample_size in range(1, sample_size + 1):
+        results_for_size = {
+            SAMPLE_SIZE_COLUMN_NAME: current_sample_size,
+            HEADS_PROBABILITY_COLUMN_NAME: heads_probability,
+            HEADS_COLUMN_NAME: 0,
+            TAILS_COLUMN_NAME: 0,
+        }
+        for y in range(1, current_sample_size + 1):
+            toss_result = coin_toss(heads_probability)
+            if(toss_result == HEADS_COLUMN_NAME):
+                results_for_size[HEADS_COLUMN_NAME] += 1
+            else:
+                results_for_size[TAILS_COLUMN_NAME] += 1
 
-    # convert the count to a probability
-    results[HEADS_COLUMN_NAME] = results[HEADS_COLUMN_NAME] / sample_size
-    results[TAILS_COLUMN_NAME] = results[TAILS_COLUMN_NAME] / sample_size
+        # convert the count to a probability
+        results_for_size[HEADS_COLUMN_NAME] = results_for_size[HEADS_COLUMN_NAME] / \
+            current_sample_size
+        results_for_size[TAILS_COLUMN_NAME] = results_for_size[TAILS_COLUMN_NAME] / \
+            current_sample_size
+
+        # append the results for this sample size to the list of all results
+        results.append(results_for_size)
 
     # return as a dataframe for easier plotting
-    return pd.DataFrame([results])
+    return pd.DataFrame(results)
 
 
 def coin_toss(heads_probability):
@@ -75,6 +78,7 @@ def coin_toss(heads_probability):
 
 # Simulate the tosses with the values suggested by the author
 tosses = simulate_tosses(sample_size, heads_probability)
+# print(tosses)
 
 # Plot the results of the simulation
 plot_tosses(tosses)
